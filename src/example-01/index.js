05667f17beff44963aed26cb48249e12b1d0afb2
Kaio Silveira <silveira.kaio@icloud.com>
2024-12-31 14:00:00 +0100

    ex-01: introduce `createPremiumBooking` factory fn

--- src/example-01/index.js
+++ src/example-01/index.js
@@ -1,4 +1,5 @@
-import { Booking } from "./bookings/index.js";
+import { Booking } from './bookings/index.js';
+import { PremiumBooking } from './bookings/premium/index.js';
 
 export const show = { title: 'Kendrick Lamar - GNX Tour', price: 100, talkback: true };
 export const extras = { premiumFee: 50, dinner: true };
@@ -6,4 +7,8 @@ export const date = new Date(2025, 3, 2);
 
 export function createBooking(show, date) {
   return new Booking(show, date);
+}
+
+export function createPremiumBooking(show, date, extras) {
+  return new PremiumBooking(show, date, extras);
 }
--- src/example-01/index.test.js
+++ src/example-01/index.test.js
@@ -1,9 +1,17 @@
-import { createBooking, date, show } from '.';
+import { createBooking, createPremiumBooking, date, extras, show } from '.';
 import { Booking } from './bookings';
+import { PremiumBooking } from './bookings/premium';
 
 describe('createBooking', () => {
   it('should create a booking instance', () => {
     const booking = createBooking(show, date);
     expect(booking).toBeInstanceOf(Booking);
+  });
+});
+
+describe('createPremiumBooking', () => {
+  it('should create a premium booking instance', () => {
+    const booking = createPremiumBooking(show, date, extras);
+    expect(booking).toBeInstanceOf(PremiumBooking);
   });
 });
