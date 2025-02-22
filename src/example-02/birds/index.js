82926e2b3cb96e70519ca082fbac75b756aa85ab
Kaio Silveira <silveira.kaio@icloud.com>
2025-02-22 12:55:43 +0000

    ex-02: introduce `_speciesDelegate` at `Bird` with custom selection

--- src/example-02/birds/index.js
+++ src/example-02/birds/index.js
@@ -1,7 +1,10 @@
+import { EuropeanSwallowDelegate } from './european-swallow/delegate';
+
 export class Bird {
   constructor(data) {
     this._name = data.name;
     this._plumage = data.plumage;
+    this._speciesDelegate = this.selectSpeciesDelegate(data);
   }
 
   get name() {
@@ -14,5 +17,14 @@ export class Bird {
 
   get airSpeedVelocity() {
     return null;
+  }
+
+  selectSpeciesDelegate(data) {
+    switch (data.type) {
+      case 'EuropeanSwallow':
+        return new EuropeanSwallowDelegate();
+      default:
+        return null;
+    }
   }
 }
