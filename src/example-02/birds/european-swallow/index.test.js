b1fbd7158c6dd360faf30412d411c1775ac99008
Kaio Silveira <silveira.kaio@icloud.com>
2025-02-22 14:26:50 +0000

    ex-02: delete `EuropeanSwallow` subclass

--- src/example-02/birds/european-swallow/index.test.js
+++
@@ -1,8 +0,0 @@
-import { EuropeanSwallow } from './index';
-
-describe('EuropeanSwallow', () => {
-  it('should have airSpeedVelocity 35', () => {
-    const bird = new EuropeanSwallow({ type: 'EuropeanSwallow', name: 'bird' });
-    expect(bird.airSpeedVelocity).toBe(35);
-  });
-});
