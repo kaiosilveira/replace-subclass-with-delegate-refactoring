724b3346c94c9944bc64861e554928844b766254
Kaio Silveira <silveira.kaio@icloud.com>
2025-02-22 12:48:50 +0000

    ex-01: delete `PremiumBooking`

--- src/example-01/bookings/premium/index.js
+++
@@ -1,8 +0,0 @@
-import { Booking } from '../index.js';
-
-export class PremiumBooking extends Booking {
-  constructor(show, date, extras) {
-    super(show, date);
-    this._extras = extras;
-  }
-}
