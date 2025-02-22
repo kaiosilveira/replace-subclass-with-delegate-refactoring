00b266d6a9a0aaa5b25a2adae742cab000f9d225
Kaio Silveira <silveira.kaio@icloud.com>
2025-02-22 14:51:32 +0000

    ex-02: delete `AffricanSwallow` subclass

--- src/example-02/birds/affrican-swallow/index.test.js
+++
@@ -1,8 +0,0 @@
-import { AffricanSwallow } from './index';
-
-describe('AffricanSwallow', () => {
-  it('should have airSpeedVelocity 38 if number of coconuts is 1', () => {
-    const bird = new AffricanSwallow({ type: 'AffricanSwallow', numberOfCoconuts: 1 });
-    expect(bird.airSpeedVelocity).toBe(38);
-  });
-});
