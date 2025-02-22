b76480ca8ed0e5c2e287424cf72a8cfdffe13ee1
Kaio Silveira <silveira.kaio@icloud.com>
2025-02-22 14:51:10 +0000

    ex-02: stop using `AffricanSwallow` subclass at `createBird` factory

--- src/example-02/birds/_factory/index.js
+++ src/example-02/birds/_factory/index.js
@@ -1,11 +1,8 @@
 import { Bird } from '../index.js';
-import { AffricanSwallow } from '../affrican-swallow/index.js';
 import { NorwegianBlueParrot } from '../norwegian-blue-parrot/index.js';
 
 export function createBird(data) {
   switch (data.type) {
-    case 'AffricanSwallow':
-      return new AffricanSwallow(data);
     case 'NorwegianBlueParrot':
       return new NorwegianBlueParrot(data);
     default:
--- src/example-02/birds/_factory/index.test.js
+++ src/example-02/birds/_factory/index.test.js
@@ -1,14 +1,8 @@
 import { createBird } from './index';
 import { Bird } from '..';
-import { AffricanSwallow } from '../affrican-swallow';
 import { NorwegianBlueParrot } from '../norwegian-blue-parrot';
 
 describe('createBird', () => {
-  it('should create an AffricanSwallow', () => {
-    const bird = createBird({ type: 'AffricanSwallow' });
-    expect(bird).toBeInstanceOf(AffricanSwallow);
-  });
-
   it('should create a NorwegianBlueParrot', () => {
     const bird = createBird({ type: 'NorwegianBlueParrot' });
     expect(bird).toBeInstanceOf(NorwegianBlueParrot);
