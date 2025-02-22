02a8dc5a3cbdb71f17a194a825729a9ea381c1df
Kaio Silveira <silveira.kaio@icloud.com>
2025-02-22 15:24:27 +0000

    ex-02: stop resolving `NorwegianBlueParrot` subclass at `createBird` factory fn

--- src/example-02/birds/_factory/index.js
+++ src/example-02/birds/_factory/index.js
@@ -1,10 +1,7 @@
 import { Bird } from '../index.js';
-import { NorwegianBlueParrot } from '../norwegian-blue-parrot/index.js';
 
 export function createBird(data) {
   switch (data.type) {
-    case 'NorwegianBlueParrot':
-      return new NorwegianBlueParrot(data);
     default:
       return new Bird(data);
   }
--- src/example-02/birds/_factory/index.test.js
+++ src/example-02/birds/_factory/index.test.js
@@ -1,13 +1,7 @@
 import { createBird } from './index';
 import { Bird } from '..';
-import { NorwegianBlueParrot } from '../norwegian-blue-parrot';
 
 describe('createBird', () => {
-  it('should create a NorwegianBlueParrot', () => {
-    const bird = createBird({ type: 'NorwegianBlueParrot' });
-    expect(bird).toBeInstanceOf(NorwegianBlueParrot);
-  });
-
   describe('type is unknown', () => {
     it('should create a Bird', () => {
       const bird = createBird({ type: 'Unknown' });
